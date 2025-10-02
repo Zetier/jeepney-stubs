838469b9342717b3879cd5f2efc0a5a7995b44b2
Yusuf Bham <yusuf.bham@zetier.com>
2025-10-02 19:14:54 -0400

    Add bus and fix re-exports

--- __init__.pyi
+++ __init__.pyi
@@ -1,21 +1,22 @@
+# mypy requires re-exports to be of the form from X import Y as Y
+from .auth import AuthenticationError as AuthenticationError
+from .auth import FDNegotiationError as FDNegotiationError
+
+from .low_level import Endianness as Endianness
+from .low_level import Header as Header
+from .low_level import HeaderFields as HeaderFields
+from .low_level import Message as Message
+from .low_level import MessageFlag as MessageFlag
+from .low_level import MessageType as MessageType
+from .low_level import Parser as Parser
+from .low_level import SizeLimitError as SizeLimitError
+
+from .bus import find_session_bus as find_session_bus
+from .bus import find_system_bus as find_system_bus
+
+from .fds import FileDescriptor as FileDescriptor
+from .fds import NoFDError as NoFDError
+
+# re-exports using *
 from .bus_messages import *
-
-from .wrappers import (
-    DBusAddress,
-    MessageGenerator,
-    Properties,
-    ProxyBase,
-    check_bus_name,
-    check_interface,
-    check_member_name,
-)
-
-__all__ = [
-    "DBusAddress",
-    "MessageGenerator",
-    "Properties",
-    "ProxyBase",
-    "check_bus_name",
-    "check_interface",
-    "check_member_name",
-]
+from .wrappers import *
